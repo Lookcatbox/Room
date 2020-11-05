4345d272774d442b2f3c3b94b2871f883acd2b3a
Lookcatbox <1245495327@qq.com>
2020-11-05 19:25:30 +0800

    logs

---
+++ updata.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string st1;
+int main()
+{
+	freopen("line.txt","r",stdin);
+	getline(cin,st1);
+	system("git add .");
+	system(st1.c_str());
+	system("git push -u origin main"); 
+//	system(); 
+	return 0;
+}
+
